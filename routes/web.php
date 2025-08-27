241cba5b2061c910adacd8e127e001c5120425e5
yuikawa-dev <yukawa71773@gmail.com>
2025-08-27 23:19:17 +0900

    計算アプリ作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Volt::route('/calcs/{value1}/{word}/{value2}', 'calcs');
